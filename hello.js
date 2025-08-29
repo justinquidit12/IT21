e06ed18a68f05e32e6710500f633b5fd4382280b
justinquidit12 <justinquidit56@gmail.com>
2025-08-29 19:59:03 +0800

    Add math.js with basic arithmetic operations

--- hello.js
+++ hello.js
@@ -3,6 +3,7 @@ console.log("hello world");
 num1 = 2;
 num2 = 3;
 
-sum = num1 + num2
-
-console.log("the sum of" + num1 + "and" + num2 + "is" + sum);
+sum = num1 + num2;
+diff = num1 - num2;
+prod = num1 * num2;
+quo = num1 / num2;
