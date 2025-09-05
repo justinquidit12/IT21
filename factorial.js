d03deca34e357eb58b3f52102e4425b60c0330e7
justinquidit12 <justinquidit56#gmail.com>
2025-09-05 08:39:10 +0800

    Added input interface for factorial and added the factorial logic

--- factorial.js
+++ factorial.js
@@ -24,7 +24,19 @@ rl.question("Enter your choice (1-3): " , choice =>{
             console.log("Factorial");
 rl.question("please enter a number for factorial" , numStr =>{
     let num = parseInt(numStr);
+
+    if (isNaN(num) || num < 0){
+        console.log("please enter a non-negative integer");
+    } else {
+        let fact = 1;
+
+        for (let i=1; i <= num; i++){
+            fact*=i;
+    }
+    console.log("The Factorial of" + num + " is " + fact);
+}
 })
+
 
          } else if (choice === "3"){
             onsole.log("existing application");
