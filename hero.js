4f9a384cb3f40d7b862bfd250aa9fa0081ac2b76
justinquidit12 <justinquidit56@gmail.com>
2025-09-16 08:48:51 +0800

    Added mage class as inherited from hero class

--- hero.js
+++ hero.js
@@ -1,45 +1,50 @@
-class Hero{
-
-    constructor(name,health,attack){
+class Hero {
+    constructor(name, health, attack) {
         this.name = name;
-        this.health = health; 
-        this.attack = attack; 
+        this.health = health;
+        this.attack = attack;
     }
 
-    getName(){
+    getName() {
         return this.name;
     }
-    getHealth(){
+    getHealth() {
         return this.health;
     }
-    showStats(){
-        console.log("Name: "+ this.name);
-        console.log("Health: "+ this.health);
-        console.log("Attack: "+ this.attack);
-        console.log("----------------------")
+    showStats() {
+        console.log("Name: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Attack: " + this.attack);
+        console.log("----------------------");
     }
-    class Warrior extends Hero {
-        useAbility() {
-            console.log(`${this.name} user Power Strike`);
-        }
+}
+
+class Warrior extends Hero {
+    useAbility() {
+        console.log(`${this.name} uses Power Strike`);
+    }
 }
 
 class Mage extends Hero {
-    constructor(name, health, mana) {
-        super(name, health);
-        this.mana = mana; 
+    constructor(name, health, attack, mana) {
+        super(name, health, attack);
+        this.mana = mana;
+    }
+
+    useAbility() {
+        console.log(`${this.name} useds Fireball!`);
     }
 
 
-   useAbility(){
-    console.log(`${this.name} cast Fireball!`);
+showStats() {
+    super.showStats();
+    console.log("mana: " + this.mana);
+    console.log("---------------");
 }
 
 }
-
-const Thorin = new Hero("Thorin", 100, 10);
-const hero2 = new Hero("Gandalf", 120, 8);
-
+const Thorin = new Warrior("Thorin", 100, 10);
+const Gandalf = new Mage("Gandalf", 120, 8, 50);
 
 //Thorin.showStats();
 //Thorin.useAbility();
